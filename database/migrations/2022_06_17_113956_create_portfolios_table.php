763834a9efbe68380f13a6838fffcba290d6f265
Agustin Mejia <buddy.m091@gmail.com>
2022-06-17 15:34:56 -0400

    Vista de detalle de proyectos.

---
+++ database/migrations/2022_06_17_113956_create_portfolios_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->string('category')->nullable();
+            $table->string('name')->nullable();
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->text('images')->nullable();
+            $table->string('url')->nullable();
+            $table->string('tags')->nullable();
+            $table->smallInteger('views')->nullable()->default(0);
+            $table->smallInteger('likes')->nullable()->default(0);
+            $table->decimal('rating', 10, 2)->nullable()->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,13 +5,13 @@
         <meta charset="utf-8">
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-        <title>IdeaCreativa | Bienvenido</title>
-        <meta content="" name="description">
-        <meta content="" name="keywords">
+        <title>@yield('title')</title>
+
+        @yield('meta')
 
         <!-- Favicons -->
-        <link href="assets/img/favicon.png" rel="icon">
-        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+        <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
+        <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
         <!-- Google Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -19,14 +19,14 @@
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
 
         <!-- Vendor CSS Files -->
-        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-        <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-        <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
         <!-- Variables CSS Files. Uncomment your preferred color scheme -->
-        <link href="assets/css/variables.css" rel="stylesheet">
+        <link href="{{ asset('assets/css/variables.css') }}" rel="stylesheet">
         <!-- <link href="assets/css/variables-blue.css" rel="stylesheet"> -->
         <!-- <link href="assets/css/variables-green.css" rel="stylesheet"> -->
         <!-- <link href="assets/css/variables-orange.css" rel="stylesheet"> -->
@@ -35,7 +35,7 @@
         <!-- <link href="assets/css/variables-pink.css" rel="stylesheet"> -->
 
         <!-- Template Main CSS File -->
-        <link href="assets/css/main.css" rel="stylesheet">
+        <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
 
         <!-- =======================================================
         * Template Name: HeroBiz - v2.1.0
@@ -43,6 +43,8 @@
         * Author: BootstrapMade.com
         * License: https://bootstrapmade.com/license/
         ======================================================== -->
+
+        @yield('css')
     </head>
 
     <body>
@@ -59,11 +61,11 @@
 
                 <nav id="navbar" class="navbar">
                     <ul>
-                        <li><a class="nav-link scrollto" href="index.html#navbar">Inicio</a></li>
-                        <li><a class="nav-link scrollto" href="index.html#cta">Acerca de</a></li>
-                        <li><a class="nav-link scrollto" href="index.html#services">Servicios</a></li>
-                        <li><a class="nav-link scrollto" href="index.html#portfolio">Portafolio</a></li>
-                        <li><a class="nav-link scrollto" href="index.html#team">Equipo</a></li>
+                        <li><a class="nav-link scrollto" href="{{ url('/') }}#navbar">Inicio</a></li>
+                        <li><a class="nav-link scrollto" href="{{ url('/') }}#features">Acerca de</a></li>
+                        <li><a class="nav-link scrollto" href="{{ url('/') }}#services">Servicios</a></li>
+                        <li><a class="nav-link scrollto" href="{{ url('/') }}#portfolio">Portafolio</a></li>
+                        <li><a class="nav-link scrollto" href="{{ url('/') }}#team">Equipo</a></li>
                         <li class="dropdown"><a href="#"><span>Proyectos</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
                             <ul>
                             <li><a href="#" target="_blank">Restaurantes</a></li>
@@ -71,7 +73,7 @@
                             <li><a href="#" target="_blank">Ecommerce</a></li>
                             </ul>
                         </li>
-                        <li><a class="nav-link scrollto" href="index.html#contact">Contacto</a></li>
+                        <li><a class="nav-link scrollto" href="{{ url('/') }}#contact">Contacto</a></li>
                     </ul>
                     <i class="bi bi-list mobile-nav-toggle d-none"></i>
                 </nav><!-- .navbar -->
@@ -92,43 +94,43 @@
 
                     <div class="col-lg-3 col-md-6">
                         <div class="footer-info">
-                        <h3>HeroBiz</h3>
+                        <h3>Idea<span>Creativa</span></h3>
                         <p>
-                            A108 Adam Street <br>
-                            NY 535022, USA<br><br>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
+                            {{-- A108 Adam Street <br> --}}
+                            Santísima Trinidad, Bolivia<br><br>
+                            <strong>Telefonos:</strong> +591 75199157<br>
+                            <strong>Email:</strong> soporte.idea.creativa@gmail.com<br>
                         </p>
                         </div>
                     </div>
 
                     <div class="col-lg-2 col-md-6 footer-links">
-                        <h4>Useful Links</h4>
+                        <h4>Enlaces</h4>
                         <ul>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ url('/') }}">Inicio</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ url('/') }}#features">Acerca de</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ url('/') }}#services">Servicios</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ url('/') }}#portfolio">Portafolio</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ url('/') }}#team">Equipo</a></li>
                         </ul>
                     </div>
 
                     <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Our Services</h4>
+                        <h4>Proyectos</h4>
                         <ul>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="https://gerente.rest" target="_blank">GerenteRest</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="https://eshop.ideacreativa.dev" target="_blank">Eshop</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="https://phonestore.ideacreativa.dev" target="_blank">PhoneStore</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="https://facturacion.ideacreativa.dev" target="_blank">Facturación</a></li>
+                            {{-- <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li> --}}
                         </ul>
                     </div>
 
                     <div class="col-lg-4 col-md-6 footer-newsletter">
-                        <h4>Our Newsletter</h4>
-                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                        <h4>Suscribirse</h4>
+                        <p>Puedes sucribirte a nuestro boletín para recibir noticias y notificaciones de actualizaciones</p>
                         <form action="" method="post">
-                        <input type="email" name="email"><input type="submit" value="Subscribe">
+                        <input type="email" name="email"><input type="submit" value="Subscribirse">
                         </form>
 
                     </div>
@@ -141,23 +143,23 @@
                 <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
 
                     <div class="d-flex flex-column align-items-center align-items-lg-start">
-                    <div class="copyright">
-                        &copy; Copyright <strong><span>HeroBiz</span></strong>. All Rights Reserved
-                    </div>
-                    <div class="credits">
-                        <!-- All the links in the footer should remain intact. -->
-                        <!-- You can delete the links only if you purchased the pro version. -->
-                        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
-                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                    </div>
+                        <div class="copyright">
+                            &copy; Copyright <strong><span>IdeaCreativa</span></strong> {{ date('Y') }}. Todos los derechos reservados.
+                        </div>
+                        <div class="credits">
+                            <!-- All the links in the footer should remain intact. -->
+                            <!-- You can delete the links only if you purchased the pro version. -->
+                            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
+                            Diseñado por <a href="https://bootstrapmade.com/" target="_blank">BootstrapMade</a>
+                        </div>
                     </div>
 
                     <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
                     <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="https://www.facebook.com/ideacreativa2022" class="facebook"><i class="bi bi-facebook"></i></a>
                     <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                    <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
+                    <a href="https://www.youtube.com/channel/UC7WoO8aDoR7Lf6g0uwVE9TQ" class="youtube"><i class="bi bi-youtube"></i></a>
                     <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
                     </div>
 
@@ -171,14 +173,14 @@
         <div id="preloader"></div>
 
         <!-- Vendor JS Files -->
-        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/vendor/aos/aos.js"></script>
-        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="assets/vendor/php-email-form/validate.js"></script>
+        <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+        <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
         <!-- Template Main JS File -->
-        <script src="assets/js/main.js"></script>
+        <script src="{{ asset('assets/js/main.js') }}"></script>
     </body>
 </html>
